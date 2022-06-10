edbf5ce358383e1703fbf9a970d5aa58e39443f5
raviSridhar55 <sridharravi55@gmail.com>
2022-06-10 11:57:28 +0530

    initial template

---
+++ backend/Routes/auth.js
@@ -0,0 +1,6 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const router = express.router();
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import colors from "colors";
+
+dotenv.config();
+
+const app = express();
+
+// Connect DataBase
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello, This is backend" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`App is running on port ${PORT}`.yellow.bold)
+);
